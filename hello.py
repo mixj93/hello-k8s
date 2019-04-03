be87e808101bc57682380c4691127b0fcc7fbf0d
lixueli <lixueli@qiniu.com>
2019-04-03 11:41:22 +0800

    add hello.py

---
+++ hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# encoding: utf-8
+import flask
+import platform
+import datetime
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "hello from: " + platform.node() + ", timestamp: " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3003)
